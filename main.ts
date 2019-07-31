4d175189f5592a555b548a5bb7b264b67107fe2a
Shorts1999 <53516987+Shorts1999@users.noreply.github.com>
2019-07-31 14:08:09 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -33,9 +33,9 @@ namespace ArexxMatrix {
             basic.pause(delayTime)
         }
     }
-    //%block="set Matrix brightness (0-255)" setPoint.max=255 setPoint.min=0 setPoint.defl=128
+    //%block="set Matrix brightness (0-255) %setPoint" setPoint.max=255 setPoint.min=0 setPoint.defl=128
     //% 
-    export function Brightness(setPoint: number){
+    export function Brightness(setPoint: number) {
         matrix.setBrightness(setPoint)
     }
 
