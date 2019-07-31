fa79a2771bbf9aad7d9f05c77a61dba0bb0c5a1f
Shorts1999 <53516987+Shorts1999@users.noreply.github.com>
2019-07-31 12:51:15 +0200

    Added default colour value

--- main.ts
+++ main.ts
@@ -37,7 +37,7 @@ namespace ArexxMatrix {
     //% blockId="Matrix_rgb" block="red %red|green %green|blue %blue"
     //% red.min=0 red.max=255 green.min=0 green.max=255 blue.min=0 blue.max=255
     //% advanced=true
-    export function rgb(red: number, green: number, blue: number): number {
+    export function rgb(red: number=255, green: number=255, blue: number=255): number {
         let colourValue = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF)
         return colourValue;
     }
