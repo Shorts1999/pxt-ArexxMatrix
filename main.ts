4186e48b89bdeda4bf8d2e0e87fe00f0be4900d8
Shorts1999 <53516987+Shorts1999@users.noreply.github.com>
2019-10-31 14:33:08 +0100

    Set the default pin to P2

--- main.ts
+++ main.ts
@@ -22,10 +22,10 @@ namespace ArexxMatrix {
      */
     //%block="maak matrix met hoogte %Heigth| en breedte %Width| met RGB type %mode| op pin %PinNumber"
     //%weight=100
-    //% Heigth.defl=10 Width.defl=20 PinNumber.defl=DigitalPin.P0
+    //% Heigth.defl=10 Width.defl=20 PinNumber.defl=DigitalPin.P2
     export function createMatrix(Heigth: number, Width: number, mode: NeoPixelMode, PinNumber: DigitalPin): void {
         if (!matrix) {
-            matrix = neopixel.create(DigitalPin.P5, Heigth * Width, mode)
+            matrix = neopixel.create(PinNumber, Heigth * Width, mode)
             ArexxMatrix.Brightness(32)
             MatrixHeigth = Heigth
             MatrixWidth = Width
